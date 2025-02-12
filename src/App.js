dc978f20248692a8f32ee065eeafe2ca201d6f99
fal hamza <fal.hamza98@gmail.com>
2025-02-12 18:55:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+
+    <>
+      <h1 className="ham">hamza</h1>
+      <h2 className="ham">fal</h2>
+      <p className="ham">q ubxbc uxnauba xahxuaxqa xq </p>
+    </>
+  );
+}
+
+export default App;
